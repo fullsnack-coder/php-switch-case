efb67739101a56ea124ed716a11598eb3092288f
Manuel Garcia <manueldev250799@gmail.com>
2023-05-22 03:57:29 -0500

    update html

--- procesar.php
+++ procesar.php
@@ -47,7 +47,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $generoMensaje ?></title>
     <link rel="stylesheet" href="main.css">
